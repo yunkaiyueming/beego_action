661843341d013ffc0f07438ad86410a7f785b922
hongcheng.xu <819664855@qq.com>
2016-05-10 16:59:15 +0800

    增加注解路由形式

--- controllers/log.go
+++ controllers/log.go
@@ -8,6 +8,8 @@ type LogController struct {
 	beego.Controller
 }
 
+// 注解路由形式测试
+// @router /:get_log_by_id [delete]
 func (m *LogController) GetLogById() {
 	m.Ctx.WriteString("get log controller")
 }
