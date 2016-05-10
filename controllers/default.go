56e8a867302c8130489f7823c76f488709db246b
hongcheng.xu <819664855@qq.com>
2016-05-10 16:58:46 +0800

    增加mysql curd操作

--- controllers/default.go
+++ controllers/default.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/config"
-	//"github.com/go-sql-driver/mysql"
 )
 
 type MainController struct {
@@ -21,21 +20,21 @@ func (c *MainController) Get() {
 	c.Data["appname"] = beego.AppConfig.String("appname")
 	c.Data["static_url"] = beego.AppConfig.String("static_url")
 	c.Data["test_str"] = beego.AppConfig.String("prod::test_str")
-	record_log("eme", "get user msg")
+	models.Record_log("eme", "get user msg")
 
 	c.TplName = "index.tpl"
+	c.Render()
 }
 
-func (c *MainController) GetUser() {
+func (c *MainController) GetAllUser() {
 	u := &models.UserModel{}
-	users, err := u.GetAllUser("test", "users")
-	CheckError(err)
-
+	users, err := u.GetAllUser()
+	models.CheckError(err)
 	fmt.Println(users)
 
-	//c.Ctx.WriteString("hello gettest")
 	c.Data["users"] = users
 	c.TplName = "user/get_user.tpl"
+	c.Render()
 }
 
 func (c *MainController) UpdateUser() {
@@ -57,15 +56,6 @@ func (c *MainController) DeleteUser() {
 	c.Ctx.WriteString("hello deleteuser")
 }
 
-func record_log(msg_level, msg string) {
-	beego.SetLogger("file", `{"filename":"E:/GO_PATH/src/beego_code/test.log"}`)
-	//beego.BeeLogger.DelLogger("console")
-	beego.SetLevel(beego.LevelInformational)
-	beego.SetLogFuncCall(true)
-
-	beego.Emergency(msg)
-}
-
 func (c *MainController) GetConfigData() {
 	iniconf, err := config.NewConfig("ini", "conf/app.conf")
 	if err != nil {
@@ -78,25 +68,4 @@ func (c *MainController) GetConfigData() {
 		panic("appid is wrong")
 	}
 	c.TplName = "get_config_data.tpl"
-}
-
-func CheckError(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
-func (c *MainController) GetDbConfig() map[string]string {
-	iniconf, err := config.NewConfig("ini", "conf/app.conf")
-	if err != nil {
-		panic("get app config is wrong")
-	}
-
-	DbConfigInfo := make(map[string]string)
-	DbConfigInfo["DbHost"] = iniconf.String("mysqlurls")
-	DbConfigInfo["User"] = iniconf.String("mysqluser")
-	DbConfigInfo["Pwd"] = iniconf.String("mysqlpass")
-	DbConfigInfo["DbName"] = iniconf.String("mysqldb")
-
-	return DbConfigInfo
 }
